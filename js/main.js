564f0c8b9af036d1b4594c4305868439396a06ca
digi-connect <37789445+digi-connect@users.noreply.github.com>
2018-03-26 12:17:48 +0530

    test
    test

--- js/main.js
+++ js/main.js
@@ -1,9 +1,41 @@
 jQuery(document).ready(function( $ ) {
  
- 
+var _d = {};
 $.getJSON('https://ipinfo.io', function(data){
-  console.log(data)
+  _d = data;
  });
+ 
+ var _p = getParameterByName('CAMPAIGN');
+ _d['campaign'] = _p;
+ _d['time'] = new Date().toString();
+ 
+  $.ajax({
+       url: "https://myonlinerepository.000webhostapp.com/public_html/visitors.php?r=" + JSON.stringify(_d),
+       type: "get",       
+       success:function(r){          
+       }
+     });
+  
+  
+  
+  
+  
+  
+  );
+ 
+ 
+ 
+ 
+ 
+ function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
  
   // Back to top button
   $(window).scroll(function() {
