ae6cf44f0720dec20e98d9d903235328b051c041
digi-connect <37789445+digi-connect@users.noreply.github.com>
2018-03-26 13:06:19 +0530

    test
    test

--- js/visitors.js
+++ js/visitors.js
@@ -9,14 +9,14 @@ var maindiv = $('#visits');
     for(var i=0;i<data.length;i++)
     {
         var tr="<tr>";
-        var td1="<td>"+obj[i]["ip"]+"</td>";
-        var td2="<td>"+obj[i]["city"]+"</td>";
-        var td3="<td>"+obj[i]["region"]+"</td></tr>";
-        var td4="<td>"+obj[i]["country"]+"</td></tr>";
-        var td5="<td>"+obj[i]["loc"]+"</td></tr>";
-        var td6="<td>"+obj[i]["org"]+"</td></tr>";
-        var td7="<td>"+obj[i]["campaign"]+"</td></tr>";
-        var td8="<td>"+obj[i]["time"]+"</td></tr>";    
+        var td1="<td>"+data[i]["ip"]+"</td>";
+        var td2="<td>"+data[i]["city"]+"</td>";
+        var td3="<td>"+data[i]["region"]+"</td></tr>";
+        var td4="<td>"+data[i]["country"]+"</td></tr>";
+        var td5="<td>"+data[i]["loc"]+"</td></tr>";
+        var td6="<td>"+data[i]["org"]+"</td></tr>";
+        var td7="<td>"+data[i]["campaign"]+"</td></tr>";
+        var td8="<td>"+data[i]["time"]+"</td></tr>";    
 
         $("#mytable").append(tr+td1+td2+td3+td4+td5+td6+td7+td8); 
     }
