0e9fef3fda184c675a3dfd2f860010847a7e726f
digi-connect <37789445+digi-connect@users.noreply.github.com>
2018-03-26 13:26:51 +0530

    test
    test

--- js/visitors.js
+++ js/visitors.js
@@ -1,7 +1,9 @@
 
 jQuery(document).ready(function( $ ) {
  $.getJSON( "https://myonlinerepository.000webhostapp.com/public_html/visitsinfo.php", function( data ) {      
-    for(var i=0;i<data.length;i++)
+    
+  var tbody = "";
+  for(var i=0;i<data.length;i++)
     {
         var tr="<tr>";
         var td1="<td>"+data[i]["ip"] || '' +"</td>";
@@ -13,8 +15,13 @@ jQuery(document).ready(function( $ ) {
         var td7="<td>"+data[i]["campaign"] || ''+"</td></tr>";
         var td8="<td>"+data[i]["time"] || ''+"</td></tr>";    
 
-        $("#mytable").append(tr+td1+td2+td3+td4+td5+td6+td7+td8); 
+        var _tr = tr+td1+td2+td3+td4+td5+td6+td7+td8;
+        tbody = tbody + _tr;
     }
+  
+  var tbl = '<thead> <tr> <th>IP</th> <th>CITY</th> <th>REGION</th> <th>COUNTRY</th> <th>LOCATION</th> <th>ORGANIZATION</th> <th>CAMPAIGN</th> <th>TIME</th> </tr> </thead>'+
+              '<tbody>' + tbody + '</tbody>';
+  $('#mytable').append(tbl);
+  
+   }); 
 });
-
- });
